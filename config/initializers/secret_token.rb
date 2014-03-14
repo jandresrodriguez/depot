ce7ac36bab6cc638bce0ee2b32f710e13a2d1e5a
Juan Andres Rodriguez <enanosanlo@gmail.com>
2014-03-14 20:19:55 -0300

    Validation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookApp::Application.config.secret_key_base = 'a49faa9a7cff46f69c16e819458d63ab23e8a67c7112bb03fcd97ee717805cda386c823399c5a6e039c15619b22c4f9f7d0b8640f26cc431a7b2b24e7161708f'
